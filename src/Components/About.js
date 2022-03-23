aab23ac7b3c909bbc1e35ffdbea2ecdb13c0a6cf
Gulnihal Naldoken <gh.naldoken@gmail.com>
2022-03-23 10:41:27 +0000

    change button

--- src/Components/About.js
+++ src/Components/About.js
@@ -50,8 +50,8 @@ class About extends Component {
                 </div>
                 <div className="columns download">
                   <p>
-                    <a href={resumeDownload} className="button">
-                      <i className="fa fa-download"></i>Download Resume
+                    <a href={resumeDownload} className="button" target="_blank">
+                      Open Resume
                     </a>
                   </p>
                 </div>
