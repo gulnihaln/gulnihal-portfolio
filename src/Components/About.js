b5009426599982ecfc5212e0a37d2ac7d9a9a9f9
Gulnihal Naldoken <gh.naldoken@gmail.com>
2022-04-01 01:04:36 +0100

    update about

--- src/Components/About.js
+++ src/Components/About.js
@@ -51,7 +51,7 @@ class About extends Component {
                 <div className="columns download">
                   <p>
                     <a href={resumeDownload} className="button" target="_blank">
-                      Open Resume
+                      Resume
                     </a>
                   </p>
                 </div>
