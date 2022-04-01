ef37925bce7888c5e747f1ab38666451f5e133fa
Gulnihal Naldoken <gh.naldoken@gmail.com>
2022-04-01 01:03:56 +0100

    update header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -67,11 +67,13 @@ class Header extends Component {
             <hr />
             <Fade bottom duration={2000}>
               <ul className="social">
-                <a href={project} className="button btn project-btn">
-                  <i className="fa fa-book"></i>Project
+                <a href={project} className="button btn project-btn" target="_blank">
+                  {/* <i className="fa fa-linkedin" aria-hidden="true"></i> */}
+                  LinkedIn
                 </a>
-                <a href={github} className="button btn github-btn">
-                  <i className="fa fa-github"></i>Github
+                <a href={github} className="button btn github-btn" target="_blank">
+                  {/* <i className="fa fa-github"></i> */}
+                  GitHub
                 </a>
               </ul>
             </Fade>
